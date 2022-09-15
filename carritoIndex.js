6e52fd2c6c2ef0a2bd8526de9f33bb0b5cfec827
Martin Ponce <mdp.mclneurodiagnostic@gmail.com>
2022-09-14 22:48:51 -0300

    incorporar eventos

---
+++ carritoIndex.js
@@ -0,0 +1,35 @@
+const carritoDeCompras = []
+
+const contadorCarrito = document.getElementById("contador-carrito");
+
+const carritoIndex = (productoId)=>{
+
+    const contenedorCarrito = document.getElementById("carrito-contenedor")
+
+    const renderProductosCarrito = ()=> {
+        let producto  = productos.find( producto => producto.id == productoId )
+        carritoDeCompras.push(producto)
+        console.log(carritoDeCompras);
+
+        producto.cantidad = 1
+
+        let div = document.createElement("div")
+        div.classList.add("productoEnCarrito")
+
+        div.innerHTML = `<p>${producto.nombre}</p>
+                        <p>Precio: ${producto.precio}</p> 
+                        <p id="cantidad${producto.id}">Cantidad: ${producto.cantidad}</p>
+                        <button onclick="eliminarDelCarrito(${producto.id})" class="boton-eliminar" ><i class="fa-solid fa-trash-can"></i></button>`;
+        contenedorCarrito.appendChild(div);
+        contadorCarrito.innerText = carritoDeCompras.length;
+    }
+
+    renderProductosCarrito()
+}
+
+const eliminarDelCarrito = (productoId) => {
+    const item = carritoDeCompras.find( (producto) => producto.id == productoId );
+    const indice = carritoDeCompras.indexOf(item);
+    carritoDeCompras.splice(indice, 1);
+    renderProductosCarrito()
+}
---
+++ stock.js
@@ -0,0 +1,61 @@
+// Defino el objeto con los productos en stock
+
+const productos = [
+    {
+        id:1,
+        nombre: "BUZO FOX",
+        desc:"Legion packable",
+        precio: 24000,
+        img:'src/img/BUZO FOX.jpg',
+        cantidad: 10
+    },
+    {
+        id:2,
+        nombre:"Conjunto FOX hombre",
+        desc:"Equipo Fox-220 Lux",
+        precio: 43499,
+        img:'src/img/CASCO FOX.jpg',
+        cantidad: 10
+    },
+    {
+        id:3,
+        nombre:"Gorra FOX",
+        desc:"Legacy FlexFit",
+        precio: 6999,
+        img:'src/img/FOX GORRA.jpg',
+        cantidad: 5
+    },
+    {
+        id:4,
+        nombre:"Conjunto FOX mujer",
+        desc:"Equipo Fox-180 Trev",
+        precio: 42499,
+        img:'src/img/INDUMENTARIA FOX MUJER.jpg',
+        cantidad: 6
+    },
+    {
+        id:5,
+        nombre:"Remera FOX mujer",
+        desc:"Dier SS T-shirt",
+        precio: 11000,
+        img:'src/img/REMERA FOX MUJER.jpg',
+        cantidad: 3
+    },
+    {
+        id:6,
+        nombre:"Equipo FOX",
+        desc:"Pechera Motocross-Enduro",
+        precio: 18000,
+        img:'src/img/EQUIPO FOX HOMBRE.jpg',
+        cantidad: 12
+    },
+    {
+        id:7,
+        nombre:"Conjunto FOX Motocross Mujer",
+        desc:"Trice 22 Motocross-Enduro",
+        precio: 47000,
+        img:'src/img/EQUIPO FOX MUJER.jpg',
+        cantidad: 8
+    }
+]
+Footer
